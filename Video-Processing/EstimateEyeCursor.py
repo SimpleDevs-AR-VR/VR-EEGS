81461c52c4e383af62d8d35cdbae6259ac3a0846
Ryan Kim <kimryan0416@gmail.com>
2024-02-23 12:50:13 -0500

    tabling an issue where the extract timestamps are weird...

---
+++ Video-Processing/EstimateEyeCursor.py
@@ -0,0 +1,101 @@
+import json
+import argparse
+import pandas as pd
+import cv2
+import numpy as np
+import os
+import csv
+
+def EstimateCursor(
+        source_filepath, 
+        events_filepath, 
+        mapping_filepath, 
+        frame_timestamps_filepath
+            ):
+    # Note: `sim_start_ts` is expected to be in unix seconds
+
+    # Step 1: Read the mapping data
+    with open(mapping_filepath) as jsonfile:
+        mapping = json.load(jsonfile)
+
+    # Step 2: Get the transformation matrix from the mapping data. Define the transformation defintion
+    transformation_matrix = np.array(mapping['transformation_matrix'])
+    def transform(input):
+        A = np.array(input + [1])
+        return np.dot(A,transformation_matrix)
+
+    # Read the events data
+    events_df = pd.read_csv(events_filepath)
+    events_start = events_df.iloc[0]['unix_ms']
+    print(events_start)
+    eye_df = events_df[
+        (events_df['event_type'] == 'Eye Tracking') 
+        & (events_df['description'] == 'Screen Position')
+        & (events_df['title'] == 'Left')
+    ]
+    eye_df['unix_rel'] = eye_df['unix_ms'].apply(lambda x: (x-events_start)/1000)
+    print(eye_df.head(20))
+
+    # Read the frame timestamps data
+    # note that all the timestamps are in seconds here. Hence the /1000 division above
+    frame_ts_df = pd.read_csv(frame_timestamps_filepath)
+
+    root_dir = os.path.dirname(source_filepath)
+    out_filename = os.path.splitext(os.path.basename(source_filepath))[0]
+    out_vidpath = os.path.join(root_dir, f'{out_filename}_eyecursor.avi')
+    out_csvpath = os.path.join(root_dir, f'{out_filename}_eycursor.csv')
+
+    cap = cv2.VideoCapture(source_filepath)
+    capw  = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))   # float `width`
+    caph = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))   # float `height`
+    capfps = int(cap.get(cv2.CAP_PROP_FPS))          # FPS
+    out = cv2.VideoWriter(out_vidpath, cv2.VideoWriter_fourcc('M','J','P','G'), capfps, (capw,caph))
+
+    fields = ['frame', 'x', 'y']
+    with open(out_csvpath, 'w') as csvfile:
+        # creating a csv writer object
+        csvwriter = csv.writer(csvfile) 
+        # writing the fields
+        csvwriter.writerow(fields) 
+
+        prev_timestamp = 0.0
+        frame_counter = -1
+        eye_pos = None
+        while(cap.isOpened()):
+            success, frame = cap.read()
+            if success:
+                frame_counter += 1
+                frame_row = frame_ts_df[frame_ts_df['frame'] == frame_counter].iloc[0]
+                frame_ts = frame_row['timestamp']
+                result = np.copy(frame)
+                #result = cv2.drawMarker(result, [int(img_center[0]),int(img_center[1])], (0,255,255), cv2.MARKER_CROSS, 20, 2)
+                # print("for frame : " + str(frame_no) + "   timestamp is: ", str(cap.get(cv2.CAP_PROP_POS_MSEC)))
+                print(f'{prev_timestamp} - {frame_ts}')
+                eye_positions = eye_df[(eye_df['unix_rel'] >= prev_timestamp) & (eye_df['unix_rel'] < frame_ts)]
+                if not eye_positions.empty:
+                    ep = eye_positions.loc[eye_positions.index[0]]
+                    eye_pos_est = transform([ep['x'], ep['y']])
+                    eye_pos = (int(eye_pos_est[0]), int(caph-eye_pos_est[1]))
+                    # we only update the csv if the eye position is new
+                    csvwriter.writerow([frame_counter, eye_pos[0], eye_pos[1]])
+                if eye_pos is not None:
+                    result = cv2.drawMarker(result, eye_pos, (255,0,0), cv2.MARKER_CROSS, 20, 2)
+                out.write(result)
+                prev_timestamp = frame_ts
+            else:
+                break
+
+    cap.release()
+    out.release()
+
+    return out_vidpath, out_csvpath
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source',help='The footage that needs to have the cursor estimated on.')
+    parser.add_argument('events',help='The CSV file generated from the vr simulation that contains eye positions')
+    parser.add_argument('mapping',help='The mapping data that contains estimations of anchor positions')    
+    parser.add_argument('frame_timestamps',help='The CSV file that contains the timestamps of each frame')
+    args = parser.parse_args()
+
+    EstimateCursor(args.source, args.events, args.mapping, args.frame_timestamps)
--- Video-Processing/app.py
+++ Video-Processing/app.py
@@ -1,22 +1,71 @@
 import os
+import subprocess
 import argparse
-from ExtractFrameTimestamps import ExtractTimestamps
+import pandas as pd
+import datetime
 
-def ProcessFootage(root, camera, mappings, vr_events, side="left", verbose=True):
+from ExtractFrameTimestamps import ExtractTimestamps
+from EstimateEyeCursor import EstimateCursor
+
+def ms_to_hours(millis, include_millis=True):
+    seconds, milliseconds = divmod(millis, 1000)
+    if len(str(seconds)) == 1:  seconds = f'0{seconds}'
+    if len(str(milliseconds)) == 1: milliseconds = f'00{milliseconds}'
+    elif len(str(milliseconds)) == 2: milliseconds = f'0{milliseconds}'
+    minutes, seconds = divmod(seconds, 60)
+    hours, minutes = divmod(minutes, 60)
+    if len(str(minutes)) == 1: minutes = f'0{minutes}'
+    if len(str(hours)) == 1: hours = f'0{hours}'
+    if include_millis:
+        return f"{hours}:{minutes}:{seconds}.{milliseconds}"
+    return f"{hours}:{minutes}:{seconds}"
+
+
+def ProcessFootage(root, camera, mappings, vr_events, camera_start_ms, verbose=True):
 
     # Step 1: Build relative URLs for each of the necessary files
     camera_path = os.path.join(root, camera)
+    camera_ext = os.path.splitext(camera)[1]
     mappings_path = os.path.join(root, mappings)
     vr_events_path = os.path.join(root, vr_events)
 
-    # Step 2: Extract the timestamps and save them
-    timestamps_path = ExtractTimestamps(camera_path, False, verbose)
+    # Step 2: Trip the camera footage based on some calculations involving the start time of the video and the start time of the simulation
+    # How to trim: we need the starting position (in hh:mm:ss.milli) and the end (in hh:mm:ss.milli)
+    # To calculate the start, we perform: simulation_start - camera_start_ms
+    # We get `simulation_start` from `vr_events.csv` - the first row value
+    events_df = pd.read_csv(vr_events_path)
+    #simulation_start_row = events_df[(events_df['event_type'] == 'Simulation') & (events_df['title'] == 'Trial 1 Start')]
+    #simulation_start = simulation_start_row['unix_ms'].iloc[0]
+    simulation_start = events_df.iloc[0]['unix_ms']
+    simulation_end = events_df.iloc[-1]['unix_ms']
+    #trim_start_ms = ms_to_hours(simulation_start - camera_start_ms)
+    #trim_end_ms = ms_to_hours(simulation_end - camera_start_ms)
+    trim_start_ms = (simulation_start - camera_start_ms)/1000
+    trim_end_ms = (simulation_end - camera_start_ms)/1000
+    trim_duration = trim_end_ms - trim_start_ms
+    trim_path = os.path.join(root, f'trim{camera_ext}')
+    #cmd_str = f'ffmpeg -i {camera_path} -c copy -ss {trim_start_ms} -to {trim_end_ms} {trim_path}'
+    cmd_str = f'ffmpeg -i {camera_path} -vf "trim={trim_start_ms}:{trim_end_ms}" -af "atrim=start={trim_start_ms}:{trim_end_ms}" -vsync 2 {trim_path}'
+    _ = subprocess.check_output(cmd_str, shell=True)
 
-    # Step 3: Correct the footage
-    #cmd_str = f"ffprobe -loglevel error -select_streams v:0 -show_entries packet=pts_time,flags -of json {args.input}"
-    #output = json.loads(subprocess.check_output(cmd_str, shell=True).decode('utf-8'))
+    # Step 3: Extract the timestamps and save them
+    timestamps_path = ExtractTimestamps(trim_path, True, verbose)
 
+    # Step 4: Correct the footage
+    # We'll get the left eye for this one
+    # We'll be performing cropping, rotatingm, and lens correction via Hugin's method
+    left_path = os.path.join(root, f'left{camera_ext}')
+    #cmd_str = f'ffmpeg -i {trim_path} -vf "crop=632:672:16:0,rotate=21*(PI/180),lenscorrection=cx=0.57:cy=0.51:k1=-0.48:k2=0.2" -vsync 2 {left_path}'
+    #_ = subprocess.check_output(cmd_str, shell=True)
+    corrected_timestamps_path = ExtractTimestamps(left_path, True, verbose)
 
+    # Step 4: eye cursor estimation
+    # For each frame, we need to estimate what the position of the eye might be
+    # We'll use `EstimateEyeCusor` for this
+    cursor_vidpath, cursor_csvpath = EstimateCursor(left_path, vr_events_path, mappings_path, timestamps_path)
+    cursor_timestamps_path = ExtractTimestamps(cursor_vidpath, True, verbose)
+
+    
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -24,11 +73,20 @@ if __name__ == "__main__":
     parser.add_argument('camera', help="The filename of the raw camera footage. It must be the RAW footage, aka nothing has been croppped or anything.")
     parser.add_argument("mappings", help="The filename of the mappings for eye cursor correction.")
     parser.add_argument("vr_events", help="The filename of the csv file that stores the events captured from the VR simulation app.")
-    parser.add_argument("-s", "--side", help="Which eye (left or right) should we look at?", choices={"left", "right"})
+    
+    parser.add_argument("camera_start_ms", help="The start time (unix milliseconds) of when the camera feed was recorded. AKA at what unix millisecond was the camera footage started at?", type=int)
+    #parser.add_argument("camera_sim_start", help="The time (in seconds) when the Unity logo first appears at the start of the simulation.")
     parser.add_argument('-v', '--verbose', help="Should we be verbose in printing out statements?", action="store_true")
     args = parser.parse_args()
 
     print(args)
 
-    ProcessFootage(args.root, args.camera, args.mappings, args.vr_events, args.verbose)
+    ProcessFootage(
+        args.root, 
+        args.camera, 
+        args.mappings, 
+        args.vr_events, 
+        args.camera_start_ms, 
+        args.verbose
+            )
 
