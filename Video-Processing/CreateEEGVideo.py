871154b06213fec6411839b4fe5f4073ff053dc0
Ryan Kim <kimryan0416@gmail.com>
2024-02-29 16:39:10 -0500

    corrected how the eeg is generated

--- Video-Processing/CreateEEGVideo.py
+++ Video-Processing/CreateEEGVideo.py
@@ -50,25 +50,50 @@ def thresholding_algo(y, lag, threshold, influence):
                 avgFilter = np.asarray(avgFilter),
                 stdFilter = np.asarray(stdFilter))
 
-def CreateEEGVideo(eeg_path, output_dir, start_timestamp, end_timestamp, timestamps_path, fps=60, verbose=True):
+def CreateEEGVideo(
+        eeg_path, 
+        output_dir, 
+        start_timestamp, end_timestamp, 
+        timestamps_list = None, 
+        display_xlims = [0.5,80],
+        display_ylims = [0,200],
+        fps=60, 
+        output_csvname="eeg_trimmed", output_vidname="psd", 
+        verbose=True
+            ):
 
-    output_trimmed_csv = os.path.join(output_dir,'eeg_trimmed.csv')
-    psd_videopath = os.path.join(output_dir, 'psd.avi')
+    """ ===========================================
+    === Step 1: Determine the output file paths ===
+    =========================================== """
+    output_trimmed_csv = os.path.join(output_dir,f'{output_csvname}.csv')
+    psd_videopath = os.path.join(output_dir, f'{output_vidname}.avi')
 
-    """ ==============================================================
-    === Step 7: Limit the EEG data range based on video start time ===
-    ============================================================== """
-    start_timestamp_sec = start_timestamp/1000
-    end_timestamp_sec = end_timestamp/1000
+
+    """ ==========================================
+    === Step 2: Read the original EEG raw data ===
+    ========================================== """
     eeg_df = pd.read_csv(eeg_path)
     eeg_df.rename(columns={'ch1':'TP9', 'ch2':'AF7', 'ch3':'AF8', 'ch4':'TP10', 'ch5':'AUX'}, inplace=True)
     eeg_df = eeg_df.drop_duplicates()
+
+
+    """ ======================================================================
+    === Step 3: Limit the EEG data range based on video start and end time ===
+    ====================================================================== """
+    start_timestamp_sec = start_timestamp/1000
+    end_timestamp_sec = end_timestamp/1000
     eeg_df = eeg_df[(eeg_df['unix_ts'] >= start_timestamp_sec) & (eeg_df['unix_ts'] <= (end_timestamp_sec))]
     eeg_df['unix_rel_ts'] = eeg_df['unix_ts'] - start_timestamp_sec
+
+
+    """ =======================================================
+    === Step 4: Save the trimmd version of the raw eeg data ===
+    ======================================================= """
     eeg_df.to_csv(output_trimmed_csv, index=False)
     
+
     """ =============================================
-    === Step 8: Process EEG data into mne package ===
+    === Step 5: Process EEG data into mne package ===
     ============================================= """
     eeg_start = eeg_df.iloc[0]['unix_ts']
     eeg_end = eeg_df.iloc[-1]['unix_ts']
@@ -78,6 +103,7 @@ def CreateEEGVideo(eeg_path, output_dir, start_timestamp, end_timestamp, timesta
     eeg_info = mne.create_info(["TP9","TP10","AF7", "AF8"], eeg_frequency, ch_types='eeg', verbose=False)
     s_array = np.transpose(eeg_df[["TP9", "TP10", "AF7", "AF8"]].to_numpy())
     mne_info = mne.io.RawArray(s_array, eeg_info, first_samp=0, copy='auto', verbose=False)
+    mne_info.set_eeg_reference(ref_channels=["TP9", "TP10"])
     mne_info.filter(0, 100, verbose=False)
     if verbose:
         print('eeg_start: ' + str(eeg_start))
@@ -85,7 +111,7 @@ def CreateEEGVideo(eeg_path, output_dir, start_timestamp, end_timestamp, timesta
         print('eeg_duration: ' + str(eeg_duration))
 
     """ ======================================
-    === Step 9: Parsing EEG frame-by-frame ===
+    === Step 6: Parsing EEG frame-by-frame ===
     ==========================================
     Here, we will attempt to parse the EEG at each known frame.
     The frame timings will be designated at each frame timestamp.
@@ -102,48 +128,51 @@ def CreateEEGVideo(eeg_path, output_dir, start_timestamp, end_timestamp, timesta
     
     frequencies=["delta","theta","alpha","beta","gamma"]
     frequency_bands = {
-        "delta": {"range":(0.5,4),"color":"darkgray"},
-        "theta": {"range":(4, 8),"color":"lightblue"},
+        "delta": {"range":(0.5,4),"color":"white"},
+        "theta": {"range":(4, 8),"color":"darkgrey"},
         "alpha": {"range":(8, 16),"color":"blue"},
         "beta":  {"range":(16, 32),"color":"orange"},
         "gamma": {"range":(32, 80),"color":"red"}
     }
-    frame_timestamps = pd.read_csv(timestamps_path)
-    frame_timestamps_list = frame_timestamps['timestamp'].to_list()
-    for eeg_current_start in frame_timestamps_list:
-        eeg_current_end = eeg_current_start + 2.0
-        if eeg_current_end > frame_timestamps_list[-1]: break
-        psd = mne_info.compute_psd(
-            tmin=eeg_current_start, 
-            tmax=eeg_current_end, 
-            average='mean', 
-            fmin=0.5,
-            fmax=60,
-            verbose=False)
-        powers, freqs = psd.get_data(picks=["AF7", "AF8"], return_freqs=True)
-        # Note: freqs is the same size as the 2D layer of `powers`. `powers`' first dimension is for each frequency channel
-        # To process, we need to look at the 2nd layer of `powers` when mapping frequencies to powers
-        peak_freqs = {}
-        peak_powers = {}
-        # frequencies = ["delta", "theta", "alpha", "beta", "gamma"]
-        for freq in frequencies:
-            peak_freqs[freq] = []
-            peak_powers[freq] = []
-        if len(powers) > 0:
-            # get through 1st layer of `powers`
-            powers_avg = np.mean(powers, axis=0)
-            peaks = thresholding_algo(powers_avg, 5, 3.5, 0.5)
-            ax.cla()
-            plt.title("Power Spectral Density\n[dt: 2]")
-            ax.set_ylim([0.0, 200.0])
-            ax.set_xlabel("Frequency (Hz)")
-            ax.set_ylabel("Power (Decibels)")
-            for f in frequencies:
-                plt.axvspan(frequency_bands[f]["range"][0], frequency_bands[f]["range"][1], color=frequency_bands[f]["color"], alpha=0.1)
-            plt.plot(freqs, powers_avg, label='psd', c='b')
-            plt.plot(freqs, peaks['signals'], label='peaks', c='r')
-            psd_filepath = os.path.join(psd_filedir, f'frame_{eeg_current_start}.png')
-            plt.savefig(psd_filepath, bbox_inches="tight")
+
+    if timestamps_list is None: timestamps_list = eeg_df['unix_rel_ts'].to_list()
+    current_frame_counter = 0
+    for eeg_current_end in timestamps_list:
+        eeg_current_start = eeg_current_end - 2.0
+        current_frame_counter += 1
+        
+        ax.cla()
+        plt.title("Power Spectral Density\n[dt: 2]")
+        ax.set_ylim(display_ylims)
+        ax.set_xlim(display_xlims)
+        ax.set_xlabel("Frequency (Hz)")
+        ax.set_ylabel("Power Spectral Density (Db/Hz)")
+        for f in frequencies:
+            plt.axvspan(frequency_bands[f]["range"][0], frequency_bands[f]["range"][1], color=frequency_bands[f]["color"], alpha=0.1)
+        
+        if eeg_current_start >= 0.0: 
+            psd = mne_info.compute_psd(
+                tmin=eeg_current_start, 
+                tmax=eeg_current_end, 
+                average='mean', 
+                fmin=display_xlims[0],
+                fmax=display_xlims[1],
+                verbose=False)
+            powers, freqs = psd.get_data(picks=["AF7", "AF8"], return_freqs=True)
+            # Note: freqs is the same size as the 2D layer of `powers`. `powers`' first dimension is for each frequency channel
+            # To process, we need to look at the 2nd layer of `powers` when mapping frequencies to powers
+            peak_freqs = {}
+            peak_powers = {}
+            # frequencies = ["delta", "theta", "alpha", "beta", "gamma"]
+            if len(powers) > 0:
+                # get through 1st layer of `powers`
+                powers_avg = np.mean(powers, axis=0)
+                #peaks = thresholding_algo(powers_avg, 5, 3.5, 0.5)
+                plt.plot(freqs, powers_avg, label='psd', c='b')
+                #plt.plot(freqs, peaks['signals'], label='peaks', c='r')
+        
+        psd_filepath = os.path.join(psd_filedir, f'frame_{current_frame_counter}.png')
+        plt.savefig(psd_filepath, bbox_inches="tight")
     
     if verbose: print("Generating video from frames...")
     # Grab all frames in our temp folder. Sort them humanly.
@@ -177,4 +206,4 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("eeg", help="The path to the raw EEG data")
     parser.add_argument("output_dir", help="The directory where we want to save the results in")
-    parser.add_argument("start")
+    parser.add_argument("start", help="The unix start timestamp (milliseconds) where the ")
