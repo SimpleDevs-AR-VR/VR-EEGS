ed1b5b29fa2733aa13aea0d4e2b8fc4f1c649f33
Ryan Kim <kimryan0416@gmail.com>
2024-03-03 13:56:01 -0500

    :updatuing the eeg video code so that w can generate the mean abs power as well

--- Video-Processing/CreateEEGVideo.py
+++ Video-Processing/CreateEEGVideo.py
@@ -60,11 +60,11 @@ _COL_DICT = {
 _EEG_CHANNELS = ["AF7","AF8","TP9","TP10"]
 _EEG_REF_CHANNELS = ["TP9", "TP10"]
 _FREQUENCY_BANDS = {
-    "delta": {"range":(0.5,4),"color":"white"},
-    "theta": {"range":(4, 8),"color":"darkgrey"},
-    "alpha": {"range":(8, 16),"color":"blue"},
-    "beta":  {"range":(16, 32),"color":"orange"},
-    "gamma": {"range":(32, 80),"color":"red"}
+    "delta": {"range":(0.5,4),"color":"white","line_color":"grey"},
+    "theta": {"range":(4, 8),"color":"darkgrey","line_color":"black"},
+    "alpha": {"range":(8, 16),"color":"blue","line_color":"purple"},
+    "beta":  {"range":(16, 32),"color":"orange","line_color":"gold"},
+    "gamma": {"range":(32, 80),"color":"red","line_color":"crimson"}
 }
 
 def GetMeanAbsolutePower(powers, freqs, freq_bands, bands=["theta","alpha","beta","gamma"]):
@@ -72,8 +72,84 @@ def GetMeanAbsolutePower(powers, freqs, freq_bands, bands=["theta","alpha","beta
     mean_abs_powers = {}
     for band in bands:
         freq_range = freq_bands[band]['range']
-        mean_abs_powers[band] = np.mean([powers[i] for i in range(len(freqs)) if freqs[i] >= freq_range[0] or freqs[i] <= freq_range[1]])
+        l = freq_range[0]
+        h = freq_range[1]
+        mean_abs_powers[band] = np.mean([powers[i] for i in range(len(freqs)) if freqs[i] >= l and freqs[i] <= h])
     return mean_abs_powers
+
+def GenerateFrame(
+        ax, title, xlabel, ylabel, plotter,
+        xlims=None, ylims=None, data=None, 
+        print_freq_bands=False, 
+        line_color_colname=None, 
+        save_frame_path=None
+            ):
+    ax.cla()
+    if xlims is not None: ax.set_xlim(xlims)
+    if ylims is not None: ax.set_ylim(ylims)
+    ax.set_title(title)
+    ax.set_xlabel(xlabel)
+    ax.set_ylabel(ylabel)
+    
+    if print_freq_bands:
+        for f in _FREQUENCY_BANDS:
+            plt.axvspan(
+                _FREQUENCY_BANDS[f]["range"][0], 
+                _FREQUENCY_BANDS[f]["range"][1], 
+                color=_FREQUENCY_BANDS[f]["color"], 
+                alpha=0.1
+            )
+        
+    if data is not None:
+        if line_color_colname is not None:
+            for f in _FREQUENCY_BANDS:
+                band = _FREQUENCY_BANDS[f]
+                indices = [i for i,x in enumerate(data['x']) if x >= band['range'][0] and x < band['range'][1]]
+                x = data['x'][indices]
+                y = data['y'][indices]
+                plotter(x, y, label=f, color=band[line_color_colname])
+        else:
+            plotter(data['x'], data['y'], label=data['label'], color=data['c'])
+
+    if save_frame_path is not None:
+        plt.savefig(save_frame_path, bbox_inches="tight")
+
+def GenerateVideoFromFrames(
+        frames_dir, 
+        output_path, 
+        fps, 
+        delete_frames_dir=False, 
+        verbose=False
+            ):
+    if verbose: print(f"Generating \"{output_path}\" from frames in {frames_dir}")
+    
+    # Grab all frames in our temp folder. Sort them humanly.
+    frames_raw = [img for img in os.listdir(frames_dir) if img.endswith(".png")]
+    frames = sort_nicely(frames_raw)
+    # We'll get the first frame and temporarily use it to determine the 
+    frame = cv2.imread(os.path.join(frames_dir, frames[0]))
+    height, width, layers = frame.shape
+
+    # Initialize the video writer    
+    video = cv2.VideoWriter(output_path, cv2.VideoWriter_fourcc(*"MJPG"), fps, (width, height))
+
+    # Iterate throgh our frames
+    try:
+        for i in range(len(frames)):
+            image = frames[i]
+            video.write(cv2.imread(os.path.join(frames_dir, image)))
+    except:
+        print("[ERROR] Something went wrong while processing the video. Ending early")
+
+    # Release the video writer
+    video.release()
+    if verbose: print("Video finished generating!")
+
+    # Delete the frames dir, if necessary
+    if delete_frames_dir:
+        shutil.rmtree(frames_dir)
+        if verbose: print("Deleted frames folder!")
+
 
 def CreateEEGVideo(
         eeg_path, 
@@ -91,14 +167,14 @@ def CreateEEGVideo(
         notch_freqs = None,
 
         display_channels = ["AF7", "AF8"],
-        display_xlims = [0.5,80],
-        display_ylims = [0,200],
+        psd_xlims = [0.5,80],
+        psd_ylims = [0,200],
         fps=60, 
         
         output_dir=None,
-        output_trimname="eeg_trim", 
-        output_vidname="psd", 
+        output_trimname="eeg_trim",  
         output_psdname="psd",
+        output_mapsname="maps",
         
         verbose=True
             ):
@@ -109,7 +185,8 @@ def CreateEEGVideo(
     if output_dir is None: output_dir = os.path.dirname(eeg_path)
     if not os.path.exists(output_dir): os.makedirs(output_dir)
     output_trimmed_csv = os.path.join(output_dir,f'{output_trimname}.csv')
-    psd_videopath = os.path.join(output_dir, f'{output_vidname}.avi')
+    psd_videopath = os.path.join(output_dir, f'{output_psdname}.avi')
+    maps_videopath = os.path.join(output_dir, f'{output_mapsname}.avi')
     psd_csvpath = os.path.join(output_dir, f'{output_psdname}.csv')
 
 
@@ -171,11 +248,15 @@ def CreateEEGVideo(
     ... might provide temporal context, we're more interested in the rapid changes in EEG signals. So the temporal context is not...
     ... going to be very useful. We'll still ahve the signal data on record for each frame anyway :shrug. """
     
+    # Generate the plt figure
     fig, ax = plt.subplots(1,1,figsize=(10,4))
     
+    # Generate temp storage for PSD frames
     psd_filedir = os.path.join(output_dir, 'temp_psd_frames')
     if os.path.exists(psd_filedir): shutil.rmtree(psd_filedir)
     os.makedirs(psd_filedir)
+
+    # Generate temp storage for MAP frames
     map_filedir = os.path.join(output_dir, 'temp_map_frames')
     if os.path.exists(map_filedir): shutil.rmtree(map_filedir)
     os.makedirs(map_filedir)
@@ -183,112 +264,116 @@ def CreateEEGVideo(
     if timestamps_list is None: timestamps_list = eeg_df['unix_rel_ts'].unique()
     current_frame_counter = 0
     for eeg_current_end in timestamps_list:
+        # Initial setup of current timestamp
         eeg_current_start = eeg_current_end - 2.0
         current_frame_counter += 1
+        psd_filepath = os.path.join(psd_filedir, f'frame_{current_frame_counter}.png')
+        maps_filepath = os.path.join(map_filedir, f'frame_{current_frame_counter}.png')
         
+        # Failure case: we're not going to get any data
         if eeg_current_start < 0.0:
-            ax.cla()
-            plt.title("Power Spectral Density\n[dt: 2]")
-            ax.set_ylim(display_ylims)
-            ax.set_xlim(display_xlims)
-            ax.set_xlabel("Frequency (Hz)")
-            ax.set_ylabel("Power Spectral Density (Db/Hz)")
-            for f in _FREQUENCY_BANDS:
-                plt.axvspan(
-                    _FREQUENCY_BANDS[f]["range"][0], 
-                    _FREQUENCY_BANDS[f]["range"][1], 
-                    color=_FREQUENCY_BANDS[f]["color"], 
-                    alpha=0.1
+            # Generate and save PSD frame
+            GenerateFrame(
+                ax, "Power Spectral Density\n[dt: 2]",
+                "Frequency (Hz)", "Power Spectral Density (Db/Hz)",
+                plt.plot,
+                xlims=psd_xlims, ylims=psd_ylims, 
+                print_freq_bands=True, 
+                save_frame_path=psd_filepath
+            )
+            # Generate and save MAP frame
+            GenerateFrame(
+                ax, "Power Spectral Density: Mean Power\n[dt: 2]",
+                "Channels", "Mean Power Spectral Density (Db/Hz)",
+                plt.bar,
+                ylims=psd_ylims,
+                print_freq_bands=False,
+                save_frame_path=maps_filepath
                 )
-            psd_filepath = os.path.join(psd_filedir, f'frame_{current_frame_counter}.png')
-            plt.savefig(psd_filepath, bbox_inches="tight")
-            
-            ax.cla()
-            plt.title("Power Spectral Density: Mean Abs. Power\n[dt: 2]")
-            ax.set_ylim(display_ylims)
-            ax.set_xlim(display_xlims)
-            ax.set_xlabel("Frequency (Hz)")
-            ax.set_ylabel("Power Spectral Density: Mean Abs. Power (Db/Hz)")
-            for f in _FREQUENCY_BANDS:
-                plt.axvspan(
-                    _FREQUENCY_BANDS[f]["range"][0], 
-                    _FREQUENCY_BANDS[f]["range"][1], 
-                    color=_FREQUENCY_BANDS[f]["color"], 
-                    alpha=0.1
-                )
-            map_filepath = os.path.join(map_filedir, f'frame_{current_frame_counter}.png')
-            plt.savefig(psd_filepath, bbox_inches="tight")
+            # Skip to next frame
             continue
         
         psd = mne_info.compute_psd(
                 tmin=eeg_current_start, 
                 tmax=eeg_current_end, 
                 average='mean', 
-                fmin=display_xlims[0],
-                fmax=display_xlims[1],
+                fmin=psd_xlims[0],
+                fmax=psd_xlims[1],
                 verbose=False
                     )
         powers, freqs = psd.get_data(picks=display_channels, return_freqs=True)
-        if current_frame_counter == 1 and verbose: print(powers, freqs)
         # Note: freqs is the same size as the 2D layer of `powers`. `powers`' first dimension is for each frequency channel
         # To process, we need to look at the 2nd layer of `powers` when mapping frequencies to powers
         if len(powers) == 0:
-            ax.cla()
-            plt.title("Power Spectral Density\n[dt: 2]")
-            ax.set_ylim(display_ylims)
-            ax.set_xlim(display_xlims)
-            ax.set_xlabel("Frequency (Hz)")
-            ax.set_ylabel("Power Spectral Density (Db/Hz)")
-            for f in _FREQUENCY_BANDS:
-                plt.axvspan(
-                    _FREQUENCY_BANDS[f]["range"][0], 
-                    _FREQUENCY_BANDS[f]["range"][1], 
-                    color=_FREQUENCY_BANDS[f]["color"], 
-                    alpha=0.1
+            # Generate and save PSD frame
+            GenerateFrame(
+                ax, "Power Spectral Density\n[dt: 2]",
+                "Frequency (Hz)", "Power Spectral Density (Db/Hz)",
+                plt.plot,
+                xlims=psd_xlims, ylims=psd_ylims, 
+                print_freq_bands=True, 
+                save_frame_path=psd_filepath
+            )
+            # Generate and save MAP frame
+            GenerateFrame(
+                ax, "Power Spectral Density: Mean Power\n[dt: 2]",
+                "Channels", "Mean Power Spectral Density (Db/Hz)",
+                plt.bar,
+                ylims=psd_ylims,
+                print_freq_bands=False,
+                save_frame_path=maps_filepath
                 )
-            psd_filepath = os.path.join(psd_filedir, f'frame_{current_frame_counter}.png')
-            plt.savefig(psd_filepath, bbox_inches="tight")
+            # Skip to next frame
             continue
 
         # get through 1st layer of `powers`
         powers_avg = np.mean(powers, axis=0)
-        plt.plot(freqs, powers_avg, label='psd', c='b')
-        psd_filepath = os.path.join(psd_filedir, f'frame_{current_frame_counter}.png')
-        plt.savefig(psd_filepath, bbox_inches="tight")
+        # Get the mean abs power from the averaged data
+        maps = GetMeanAbsolutePower(
+            powers_avg, 
+            freqs, 
+            _FREQUENCY_BANDS, 
+            bands=["theta","alpha","beta","gamma"]
+        )
+        # Generate and save PSD frame
+        GenerateFrame(
+            ax, "Power Spectral Density\n[dt: 2]",
+            "Frequency (Hz)", "Power Spectral Density (Db/Hz)",
+            plt.plot,
+            xlims=psd_xlims, ylims=psd_ylims,
+            data={"x":freqs, "y":powers_avg, "label":"psd", "c":"b"}, 
+            print_freq_bands=True, 
+            line_color_colname="line_color",
+            save_frame_path=psd_filepath
+        )
+        # Generate and save MAP frame
+        GenerateFrame(
+            ax, "Power Spectral Density: Mean Power\n[dt: 2]",
+            "Channels", "Mean Power Spectral Density (Db/Hz)",
+            plt.bar,
+            ylims=psd_ylims,
+            data={
+                "x":["Theta","Alpha","Beta","Gamma"], 
+                "y":[maps[b] for b in ["theta","alpha","beta","gamma"]],
+                "label":"map",
+                "c":[_FREQUENCY_BANDS[b]['color'] for b in ["theta","alpha","beta","gamma"]]
+            },
+            print_freq_bands=False,
+            save_frame_path=maps_filepath
+        )
+        #plt.plot(freqs, powers_avg, label='psd', c='b')
+        #psd_filepath = os.path.join(psd_filedir, f'frame_{current_frame_counter}.png')
+        #plt.savefig(psd_filepath, bbox_inches="tight")
 
         #peaks = thresholding_algo(powers_avg, 5, 3.5, 0.5)
         #plt.plot(freqs, peaks['signals'], label='peaks', c='r')
         
+    GenerateVideoFromFrames(psd_filedir, psd_videopath, fps, delete_frames_dir=True, verbose=verbose)
+    GenerateVideoFromFrames(map_filedir, maps_videopath, fps, delete_frames_dir=True, verbose=verbose)
 
-        
-    
-    if verbose: print("Generating video from frames...")
-    # Grab all frames in our temp folder. Sort them humanly.
-    frames_raw = [img for img in os.listdir(psd_filedir) if img.endswith(".png")]
-    frames = sort_nicely(frames_raw)
-    # We'll get the first frame and temporarily use it to determine the 
-    frame = cv2.imread(os.path.join(psd_filedir, frames[0]))
-    height, width, layers = frame.shape
-    # Initialize the video writer
-    
-    video = cv2.VideoWriter(psd_videopath, cv2.VideoWriter_fourcc(*"MJPG"), fps, (width, height))
-    # Iterate throgh our frames
-    try:
-        for i in range(len(frames)):
-            image = frames[i]
-            video.write(cv2.imread(os.path.join(psd_filedir, image)))
-    except:
-        print("[ERROR] Something went wrong while processing the video. Ending early")
-    # Release the video writer
-    video.release()
-    
-    if verbose: print("Video generated. Now deleting extraneous frames from temp folder")
-    shutil.rmtree(psd_filedir)
-    
-    if verbose: print("Video finished generating!")
     plt.close()
 
-    return output_trimmed_csv, psd_videopath
+    return output_trimmed_csv, psd_videopath, maps_videopath
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -349,27 +434,27 @@ if __name__ == "__main__":
 
     """
     display_channels = ["AF7", "AF8"],
-    display_xlims = [0.5,80],
-    display_ylims = [0,200],
+    psd_xlims = [0.5,80],
+    psd_ylims = [0,200],
     fps=60,
     """
     parser.add_argument("-dc", "--display_channels",
                         help="The channels we want to restrict the visualization to.",
                         nargs="+",
                         default=["AF7","AF8"] )
-    parser.add_argument("-dlx", '--display_l_x', 
+    parser.add_argument("-dlx", '--psd_l_x', 
                         help="The lower frequency we want to restrict the visualization to.", 
                         type=float, 
                         default=0.5)
-    parser.add_argument("-dhx", '--display_h_x', 
+    parser.add_argument("-dhx", '--psd_h_x', 
                         help="The upper frequency we want to restrict the visualization to.", 
                         type=float, 
                         default=60.0 )
-    parser.add_argument("-dly", '--display_l_y', 
+    parser.add_argument("-dly", '--psd_l_y', 
                         help="The lower power we want to restrict the visualization to.", 
                         type=float, 
                         default=0.0 )
-    parser.add_argument("-dhy", '--display_h_y', 
+    parser.add_argument("-dhy", '--psd_h_y', 
                         help="The upper power we want to restrict the visualization to.", 
                         type=float, 
                         default=200.0 )
@@ -390,12 +475,12 @@ if __name__ == "__main__":
     parser.add_argument("-ot", '--out_trimname', 
                         help="The name (no extension) of the outputted csv file after calculating the PSD for each frame.", 
                         default="eeg_trim" )
-    parser.add_argument("-ov", '--out_vidname', 
-                        help="The name (no extension) of the outputted video file.", 
-                        default="psd" )
     parser.add_argument('-op', '--out_psdname', 
                         help="The name (no extension) of the outputted psd values file.",
                         default="psd" )
+    parser.add_argument("-ov", '--out_mapsname', 
+                        help="The name (no extension) of the outputted video file.", 
+                        default="maps" )
 
     parser.add_argument("-v", "--verbose",
                         help="Should we be verbose in our messaging?",
@@ -421,12 +506,12 @@ if __name__ == "__main__":
         h_freq=args.h_freq,
         notch_freqs=args.notch_freqs,
         display_channels=args.display_channels,
-        display_xlims=[args.display_l_x, args.display_h_x],
-        display_ylims =[args.display_l_y,args.display_h_y],
+        psd_xlims=[args.psd_l_x, args.psd_h_x],
+        psd_ylims =[args.psd_l_y,args.psd_h_y],
         fps=args.frames_per_second, 
         output_dir=args.out_dir,
         output_trimname=args.out_trimname, 
-        output_vidname=args.out_vidname, 
         output_psdname=args.out_psdname,
+        output_mapsname=args.out_mapsname,
         verbose=args.verbose
     )
